d53eecb8c224655d87849a54a44a119c3c3ca8e8
Anugrah N M <anugrah2011@gmail.com>
2026-05-08 23:58:58 +0100

    Add edit_reminder with selective field update

--- main.py
+++ main.py
@@ -102,7 +102,107 @@ class Reminders():
             print(f"ID: {row[0]}, Reminder Name: {row[1]}, Due Date: {row[2]}")
             
     def edit_reminder(self):
-        pass
+        while True:
+            try:
+                self.edit_id = int(input("What is the ID of the reminder that you want to edit?: ") )
+                self.update_or_delete = int(input(dedent("""
+                Select One:
+                1: Edit Reminder
+                2: Delete Reminder
+                3: Go Back
+                """)))
+                
+                match self.update_or_delete:
+                    case 1:
+                        self.update_reminder()
+                        print(f"Reminder {self.edit_id} has been updated!")
+                        break
+                    case 2:
+                        self.delete_reminder()
+                        print(f"Deleted Reminder {self.edit_id}")
+                        break
+                    case 3:
+                        break
+
+                print("") 
+            except ValueError as e:
+                print(e)
+                continue
+
+        
+
+
+
+    def update_reminder(self):
+
+        update_name_due_date_query = """
+        UPDATE REMINDERS
+        SET reminder_name = ?, due_date = ?
+        WHERE id = ?
+        """
+        update_name_only_query = """
+        UPDATE REMINDERS
+        SET reminder_name = ?
+        WHERE id = ?
+        """
+        update_due_date_only_query = """
+        UPDATE REMINDERS
+        SET due_date = ?
+        WHERE id = ?
+        """
+
+
+        print("Enter new values below. Press Enter to skip and keep it the same.")
+        try:
+            update_name = input("Reminder Name: ")
+            new_date = input("Due Date (dd/mm/yyyy): ")
+            
+            if not update_name:
+                update_name = None
+
+            if new_date:
+                dd_str, mm_str, yyyy_str = new_date.split("/")
+                dd = int(dd_str)
+                mm = int(mm_str)
+                yyyy = int(yyyy_str)
+                update_due_date = datetime.date(yyyy, mm, dd).strftime("%d/%m/%Y")
+                print(update_due_date)
+            else:
+                update_due_date = None
+                print(type(update_due_date))
+            
+
+            try:
+                if update_name and update_due_date:
+                    self.cursor_obj.execute(update_name_due_date_query, (update_name, update_due_date, self.edit_id ))
+                    self.connection_obj.commit()
+                elif update_name and not update_due_date:
+                    self.cursor_obj.execute(update_name_only_query, (update_name, self.edit_id ))
+                    self.connection_obj.commit()   
+                else:
+                    self.cursor_obj.execute(update_due_date_only_query, (update_due_date, self.edit_id ))
+                    self.connection_obj.commit()   
+
+                
+            except sqlite3.Error as e:
+                print(e)
+
+
+            
+        except ValueError as e:
+            print(str(e))
+
+
+
+    def delete_reminder(self):
+
+        delete_reminder_query = "DELETE FROM REMINDERS WHERE id = ?"
+        try:
+            self.cursor_obj.execute(delete_reminder_query, (self.edit_id, ))
+            self.connection_obj.commit()
+        except ValueError as e:
+            print(e)
+
     def close_connection(self):
         self.connection_obj.close()
     
